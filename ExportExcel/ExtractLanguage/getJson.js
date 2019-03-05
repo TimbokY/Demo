4ac0e044e991f97ed4862c65b86cf3842105e0d7
Timbok <bo.yang02@hand-china.com>
2019-03-05 19:43:13 +0800

    udpate: 更新读取文件导入Excel

---
+++ ExportExcel/ExtractLanguage/getJson.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+const { unionBy } = require('lodash');
+
+const newFileList = [];
+
+// 获取文件夹下所有文件名的相对路劲
+function readFolderFun(folderSrc) {
+  const fileList = fs.readdirSync(folderSrc);
+  for(let i = 0; i<fileList.length; i++) {
+    const stats = fs.statSync(`${folderSrc}/${fileList[i]}`)
+    var isDir = stats.isDirectory();
+    var isFile = stats.isFile();
+    if(isDir) {
+      readFolderFun(`${folderSrc}/${fileList[i]}`);
+    }
+    if(isFile) {
+      newFileList.push(`${folderSrc}/${fileList[i]}`);
+    }
+  }
+  return newFileList;
+}
+
+function readFileFun(PATH) {
+  const fileList = readFolderFun(PATH);
+  let dataString = "";
+  for(let i = 0; i<fileList.length; i++) {
+    const data = fs.readFileSync(fileList[i], 'utf8');
+    // const newData = data.replace(/[\r\n]/g, "").replace(/\s+/g, "")
+    const newData = data.replace(/\ +/g,"").replace(/[\r\n]/g,"");
+    dataString = dataString + newData;
+  }
+
+  const dataArray = dataString.split('get(`${promptCode}.').map(item => {
+      const res = item.split("')");
+      if(res[1]) {
+        return res[0]
+      }
+    }
+  ).filter(item=>item);
+  let dataMap = [];
+  dataArray.forEach(item => {
+    const temp = item.split("`).d('");
+    if(temp[0] && temp[1]) {
+      dataMap = [
+        {
+          prompt_code: temp[0],
+          description: temp[1],
+        },
+        ...dataMap,
+      ];
+    }
+  });
+  fs.writeFileSync('国际化.json', JSON.stringify(dataMap));
+  return unionBy(dataMap, 'prompt_code');
+}
+
+exports.readFileFun = readFileFun;
+// readFileFun();
---
+++ ExportExcel/ExtractLanguage/setExcel.js
@@ -0,0 +1,70 @@
+// const YAML = require('yamljs');
+const excelPort = require('excel-export');
+const fs = require("fs");
+const path = require('path');
+const { readFileFun } = require('./getJson');
+
+// 国际化文件
+const PATH = '../../../../Project/hzero-platform-fe/src/routes/sfin/PayableInvoice';
+// 国际化code值
+const promptCode = 'sfin.payableInvoice';
+
+exports.write = function(req, res, next) {
+  const data = readFileFun(PATH);
+  const conf = {};
+  const filename = 'export-'; //只支持字母和数字命名
+
+  // conf.stylesXmlFile = "./excelTemplate.xml";
+  // conf.name = "平台多语言";
+  conf.cols = [
+    {
+      caption: '*prompt_id',
+      type: 'string',
+      width: 16.43,
+    }, 
+    {
+      caption: '#tenant_id',
+      type: 'number',
+      width: 16.43,
+    },
+    {
+      caption: '#prompt_key',
+      type: 'string',
+      width: 44.43,
+    },
+    {
+      caption: '#prompt_code',
+      type: 'string',
+      width: 44.43,
+    },
+    {
+      caption: 'description',
+      type: 'string',
+      width: 44.43,
+    }
+    ];
+
+  // const yamlDataKey = Object.keys(yamlData[promptCode]);
+  // const yamlDataValue = Object.values(yamlData[promptCode]);
+
+  const array = data.map((item, index)=>{
+    return ['*', 0, promptCode, item.prompt_code, item.description];
+  });
+
+
+  conf.rows = array;
+  const result = excelPort.execute(conf);
+
+  const random = `${new Date().toLocaleDateString()}`;
+
+  const uploadDir = '../Excel/';
+  const filePath = uploadDir + filename + random + ".xlsx";
+
+  fs.writeFile(filePath, result, 'binary', function(err) {
+      if (err) {
+          console.log(err);
+      }
+  });
+}
+
+exports.write();
